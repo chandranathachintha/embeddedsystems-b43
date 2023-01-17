0dcf6d7ecd0a229f17d497a841cee826f6944fc2
chandranatha <chinthachandranatha963@gmail.com>
2023-01-17 22:40:32 +0530

    2nd

--- src/components/cdata.js
+++ src/components/cdata.js
@@ -2,21 +2,21 @@ export var cdata =[
         {
             "day" : 1 ,
             "date" : "21-12-2022",
-            "topic" : "C introduction , Data types , Operators ",
+            "topic" : "C introduction,Data types,Operators",
             "passcode":"T*c5jC53",
             "url":"https://us02web.zoom.us/rec/share/r6HOixUp9y4YOpPXry8q63nXELqXHGZbbLAWJa33bxF1QV2NLIyPG5fyqXU-_9bv.46SwHWsVjkNxpxH5"
         },
         {
             "day" : 2,
             "date" : "22-12-2022",
-            "topic" : "Precedence , Arthmetic , Increment and Decrement Operators",
+            "topic" : "Arthmetic,++ and -- Operators",
             "passcode":"w9=SBP@.",
             "url":"https://us02web.zoom.us/rec/share/fHKwjBOIdZSwii6WRqEOSClfJmCM7AYcJqRlGXUHkc1POihKSjoGzmbXm5dNxfn6.nZ9zy6RyWbmzCv5i"
         },
         {
             "day" : 3 ,
             "date" : "23-12-2022",
-            "topic" : "Increment and Decrement Operators Programs",
+            "topic" : "++ and -- Operators Programs",
             "passcode":"#*N04@Vy",
             "url":"https://us02web.zoom.us/rec/share/Ic_AbGmPvJTWiMq4b0psODOYNJimvJjaNM7uM9j68QMgLGYBk_hFbmOxPRU-J6U.8PkW8q1I_Nn6QK3Z"
         },
@@ -58,28 +58,28 @@ export var cdata =[
         {
             "day" : 9 ,
             "date" : "31-12-2022",
-            "topic" : "keywords , Type casting , conditional statements",
+            "topic" : "keywords,conditional statements",
             "passcode":"QG$S9bRu",
             "url":"https://us02web.zoom.us/rec/play/qZ6lTqfB164CWWPBtWZMfkYMF4fkeqSYYOAWR3jG4LF_qka_9IOTcuYDHXEKuKd6Jxmo7eCjBX_QoCeu.tgz-4lGQGBGZgtWd?continueMode=true&_x_zm_rtaid=ZVK2qKKnSlOa1rjdmV3QXg.1673939438923.e1376077aa812a620e9c228eed79c8ca&_x_zm_rhtaid=612"
         },
         {
             "day" : 10 ,
             "date" : "2-1-2023",
-            "topic" : "goto , if-else-if , Ternary operator",
+            "topic" : "goto,if-else-if,Ternary operator",
             "passcode":"L+fG4WkE",
             "url":"https://us02web.zoom.us/rec/play/8xFh3yC4GupkeK3T59fCKN6_a0LoQ70SBsuKAX9gSpBwwCiGPnrl-90968pb92xa-qOwL3Fzl1sITmvk.WntnkKzll6ThvpUl?continueMode=true&_x_zm_rtaid=ZVK2qKKnSlOa1rjdmV3QXg.1673939438923.e1376077aa812a620e9c228eed79c8ca&_x_zm_rhtaid=612"
         },
         {
             "day" : 11 ,
             "date" : "3-1-2023",
-            "topic" : "Loops(while , do while , for) , continue ",
+            "topic" : "Loops,continue ",
             "passcode":"sK*azF8h",
             "url":"https://us02web.zoom.us/rec/play/eSsgsVN1kLmnI4taWhgSB7EHK38T7a6dPocHOItOXA4PDdt23jJ4pFbg2hohv9PXg_rkutbviuVv-l1T.sHoPc2Ewa4oiFtn0?continueMode=true&_x_zm_rtaid=ZVK2qKKnSlOa1rjdmV3QXg.1673939438923.e1376077aa812a620e9c228eed79c8ca&_x_zm_rhtaid=612"
         },
         {
             "day" : 12 ,
             "date" : "4-1-2023",
-            "topic" : "switch , float point storage ",
+            "topic" : "switch,float point storage ",
             "passcode":"1u1vf^48",
             "url":"https://us02web.zoom.us/rec/play/3wsXfAgNmLnPPtyEB_dAohtVZlmzm9nhb-e4UHFJGz8ugc4tNCml_3jpJC1Y01pKX8B0FDe2mABxsCpJ.aIuMBTBYFc8a1avH?continueMode=true&_x_zm_rtaid=ZVK2qKKnSlOa1rjdmV3QXg.1673939438923.e1376077aa812a620e9c228eed79c8ca&_x_zm_rhtaid=612"
         },
@@ -94,14 +94,14 @@ export var cdata =[
         {
             "day" : 14 ,
             "date" : "6-1-2023",
-            "topic" : "Even Numbers, Prime Numbers, extract the digit from Number",
+            "topic" : "Even & Prime Numbers,extract the digit from Number",
             "passcode":"^.*v@=k9",
             "url":"https://us02web.zoom.us/rec/play/zPrl51USQOYCq3CXKc8Wyhseox_qxM7kud00vm4_nJIXLGNj454oST2k1usZTuf1UK6MQNZJztd36J9D.Nx74xT0TSIbhoPXo?continueMode=true&_x_zm_rtaid=ZVK2qKKnSlOa1rjdmV3QXg.1673939438923.e1376077aa812a620e9c228eed79c8ca&_x_zm_rhtaid=612"
         },
         {
             "day" : 15 ,
             "date" : "7-1-2023",
-            "topic" : "Reverse a Number , pallindrome , factorial , x power n",
+            "topic" : "pallindrome,factorial, x power n",
             "passcode":"g*$6%rvn",
             "url":"https://us02web.zoom.us/rec/play/vMxFJuHojt0gI4oYUPKmJZxZn9jpc9W8C54vkiGTGw8R_ba-4pPkKhJVlieM7vuqg-BFpr8C2kRTxkY8.7q4mi4XDx0Gz9Zh7?continueMode=true&_x_zm_rtaid=ZVK2qKKnSlOa1rjdmV3QXg.1673939438923.e1376077aa812a620e9c228eed79c8ca&_x_zm_rhtaid=612"
         },
@@ -122,28 +122,28 @@ export var cdata =[
         {
             "day" : 18 ,
             "date" : "10-1-2023",
-            "topic" : "sizeof(),strlen(),string functions",
+            "topic" : "sizeof,strlen,string functions",
             "passcode":"EjK5tkz^",
             "url":"https://us02web.zoom.us/rec/play/GvpR7621hLH3pXO4NTu09c3i0sBA1C0lTe3ujyNXgSsHauroNGcRGqcNxK49RCAKBgX8v7BDOc6U8x1W.260cXVddxx_HrE9Z?continueMode=true&_x_zm_rtaid=ZVK2qKKnSlOa1rjdmV3QXg.1673939438923.e1376077aa812a620e9c228eed79c8ca&_x_zm_rhtaid=612"
         },
         {
             "day" : 19 ,
             "date" : "11-1-2023",
-            "topic" : "strlen(),strcpy(),strcat(),strupr(),strlwr()",
+            "topic" : "strlen,strcpy,strcat,strupr&strlwr",
             "passcode":"V9Br05^k",
             "url":"https://us02web.zoom.us/rec/play/TfvbczfwQwFAOAAE12b_jrja3hxzxbq62KGz0Rsbw818iXtmwdr9o5D1RCAT0kfbc7niLIEyvCVs9YUP.xAR0o7IDJzjWUFaQ?continueMode=true&_x_zm_rtaid=ZVK2qKKnSlOa1rjdmV3QXg.1673939438923.e1376077aa812a620e9c228eed79c8ca&_x_zm_rhtaid=612"
         },
         {
             "day" : 20 ,
             "date" : "12-1-2023",
-            "topic" : "strcmp(),strrev(),pallindrome string",
+            "topic" : "strcmp,strrev,pallindrome string",
             "passcode":"65bn!18E",
             "url":"https://us02web.zoom.us/rec/play/KHwv2Eq3p0Fmlrl3zbn4-wyHAg4OEuubv9T0g29eVYB6GHYEr4qXuJ0jdS9yWd-wxyqvDBbNgGdZXMi2.Y8V3QdvMjnFeNcMv?continueMode=true&_x_zm_rtaid=ZVK2qKKnSlOa1rjdmV3QXg.1673939438923.e1376077aa812a620e9c228eed79c8ca&_x_zm_rhtaid=612"
         },
         {
             "day" : 21 ,
             "date" : "13-1-2023",
-            "topic" : "strchr(),strrchr(),strtok(),atoi(),itoa()",
+            "topic" : "strchr,strrchr,strtok,atoi,itoa",
             "passcode":"CzoT%P=0",
             "url":"https://us02web.zoom.us/rec/play/g7ang_Ynm8TSamlxNO4ZrezTKKvnLJMWWzSmswEriQ11LhuJvOoLR_yVHY1mFgVj0CdIn1MlJmt8EnVC.Sp89VqlxE6VQAauk?continueMode=true&_x_zm_rtaid=ZVK2qKKnSlOa1rjdmV3QXg.1673939438923.e1376077aa812a620e9c228eed79c8ca&_x_zm_rhtaid=612"
         },
